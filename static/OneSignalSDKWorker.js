48a9a6bfb70dceb4b654b56e54f8df70ccca30b7
iwashiira <89283357+iwashiira@users.noreply.github.com>
2025-05-18 12:31:07 +0000

    update TSG LIVE! 14 CTF

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/themes/tsgctf/static/sw.js?1715940986217', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/themes/tsgctf/static/sw.js?1747571390208', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
